087d8f5c99184697ca51ea9e5b46103f63929f8a
Christopher Hernandez <cerberean661@gmail.com>
2019-09-02 18:13:59 -0700

    working towards fixing the onClick

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -12,7 +12,10 @@ class TodoList extends React.Component {
         console.log('TodoList', this.props);
         return (
             <div>
-                {this.props.todos.map(todo => <Todo key={todo.id} todo={todo}/>)}
+                {this.props.todos.map(todo => <Todo key={todo.id}
+                                                    todo={todo}
+                                                    toggleCompleted={this.props.toggleCompleted}
+                                                    />)}
             </div>
         );
     }
