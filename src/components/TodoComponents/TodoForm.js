bf41ac8d9989f764530d0b7031b8e332a0fc4393
Christopher Hernandez <cerberean661@gmail.com>
2019-09-02 19:36:42 -0700

    attempting to make Add Todo functional

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -3,12 +3,29 @@ import React from 'react';
 class TodoForm extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {
+            newtodo: ""
+        };
     }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <form>
-                <input type="text" name="newtodo" placeholder="Add a new todo"/>
-                <button type="submit">Add a new Todo</button>
+                <input type="text" 
+                        name="newtodo"
+                        placeholder="...todo"
+                        onChange={this.handleChange}
+                        value={this.state.newtodo}
+                />
+                <button type="submit">Add Todo</button>
             </form>
         )
     }
